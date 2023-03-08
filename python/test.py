21716ad604ab4256106ebc8c02d3f69e1da83636
katmfrl <rnjs2257@naver.com>
2023-03-08 10:06:31 +0900

    00308

---
+++ python/test.py
@@ -0,0 +1,22 @@
+
+
+# arr = []
+
+# for i in range(100):
+#     arr.append([])
+#     for j in range(100):
+#         arr[i].append(1)
+
+# print(arr)
+
+
+# list = [3,8,17,34,89,3234]
+
+# for i in list:
+#     if(i < 5000) and (i % 3 == 0):
+#         print(i)
+
+
+
+array = [[i*3] * 3 for i in range(3)]
+print(array)
